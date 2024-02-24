fd2ec533789af81b0dc14af73be2c42031af3fae
pratik-a-hublikar <pratik.a.hublikar@gmail.com>
2024-02-24 13:17:17 +0530

    created test case which will accept any custom delimeter

--- src/test/java/org/calc/StringCalculatorTest.java
+++ src/test/java/org/calc/StringCalculatorTest.java
@@ -41,4 +41,13 @@ public class StringCalculatorTest {
                 assertArrayEquals(new int [] {11, 15},add1);
                 assertArrayEquals(new int[]{12, 7}, add2);
         }
+
+        @Test
+        public void addMultipleStringWithCustomDelimiter(){
+                StringCalculator stringCalculator = new StringCalculator();
+                int[] add1 = stringCalculator.add("//:\n3:3:5");
+                int[] add2 = stringCalculator.add("//,\n3,4,5","//-\n3-4");
+                assertArrayEquals(new int [] {11},add1);
+                assertArrayEquals(new int[]{12, 7}, add2);
+        }
 }
