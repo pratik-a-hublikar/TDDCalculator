d2988e56b69705efef79971c65120684e905c269
pratik-a-hublikar <pratik.a.hublikar@gmail.com>
2024-02-24 12:59:51 +0530

    added test cases and then the logic to accept more than one string to add

--- src/main/java/org/calc/StringCalculator.java
+++ src/main/java/org/calc/StringCalculator.java
@@ -4,7 +4,16 @@ import org.apache.commons.lang3.StringUtils;
 
 public class StringCalculator {
 
-    public int add(String numbers){
+    public int[] add(String... numbers){
+        int[] additionList =new int[numbers.length];
+        for(int i=0;i<numbers.length;i++){
+            additionList[i]= getAddition(numbers[i]);
+
+        }
+        return additionList;
+    }
+
+    private static int getAddition(String numbers) {
         int finalAddition=0;
         if(StringUtils.isEmpty(numbers)){
             return finalAddition;
--- src/test/java/org/calc/StringCalculatorTest.java
+++ src/test/java/org/calc/StringCalculatorTest.java
@@ -4,23 +4,34 @@ package org.calc;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class StringCalculatorTest {
 
 
         @Test
         public void addEmptyString(){
                 StringCalculator stringCalculator = new StringCalculator();
-                int add = stringCalculator.add("");
-                assertEquals(0,add);
+                int[] add = stringCalculator.add("");
+                assertEquals(0,add[0]);
         }
 
         @Test
         public void addSingleString(){
                 StringCalculator stringCalculator = new StringCalculator();
-                int add1 = stringCalculator.add("3,3,5");
-                int add2 = stringCalculator.add("3,4,5");
-                assertEquals(11,add1);
-                assertEquals(12,add2);
+                int[] add1 = stringCalculator.add("3,3,5");
+                int[] add2 = stringCalculator.add("3,4,5");
+                assertEquals(11,add1[0]);
+                assertEquals(12,add2[0]);
+        }
 
+
+        @Test
+        public void addMultipleString(){
+                StringCalculator stringCalculator = new StringCalculator();
+                int[] add1 = stringCalculator.add("3,3,5","1,2,3,4,5");
+                int[] add2 = stringCalculator.add("3,4,5","");
+                assertArrayEquals(new int [] {11, 15},add1);
+                assertArrayEquals(new int[]{12, 0}, add2);
         }
 }
